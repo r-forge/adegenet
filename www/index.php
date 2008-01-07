e29041ef98d0cfb0acc54383ebc82a48d138fd14
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-07 15:53:03 +0000

    Removed som stuff.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,8 +43,6 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
-
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 <p> Information about <strong>adegenet</strong> is currently available at the <a href="http://pbil.univ-lyon1.fr/software/adegenet/" <strong>adegenet website</strong></a>. </p>
