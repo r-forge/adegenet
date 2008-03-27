82475996ed9055092bd3cd96d9c362778ddeb8a5
jombart (Mr ) <t.jombart@imperial.ac.uk>
2008-03-27 16:30:32 +0000

    Fixes for old2new (pop missing was causing an error).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/classes.R
+++ pkg/R/classes.R
@@ -742,18 +742,25 @@ setGeneric("old2new",  function(object) standardGeneric("old2new"))
 setMethod("old2new", "genind", function(object){
   x <- object
   res <- new("genind")
-
+  theoLength <- 7
+  
   res@tab <- as.matrix(x$tab)
   res@ind.names <- as.character(x$ind.names)
   res@loc.names <- as.character(x$loc.names)
   res@loc.nall <- as.integer(x$loc.nall)
   res@loc.fac <- as.factor(x$loc.fac)
   res@all.names <- as.list(x$all.names)
-  res@pop <- as.factor(x$pop)
-  res@pop.names <- as.character(x$pop.names)
+  if(!is.null(x$pop)) {
+      res@pop <- as.factor(x$pop)
+      theoLength <- theoLength + 1
+  }
+  if(!is.null(x$pop.names)) {
+      res@pop.names <- as.character(x$pop.names)
+      theoLength <- theoLength + 1
+  }
   res@call <- match.call()
 
-  if(length(object)>9) warning("optional content else than pop and pop.names was not converted")
+  if(length(object) > theoLength) warning("optional content else than pop and pop.names was not converted")
 
   return(res)
 })
