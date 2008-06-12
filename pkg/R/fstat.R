bb4d3d78f1c541e04eee3e157ec31c27dee2df7c
jombart (Mr ) <t.jombart@imperial.ac.uk>
2008-06-12 10:05:28 +0000

    Added a few as methods. Started a function for classical fst computation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fstat.R
+++ pkg/R/fstat.R
@@ -1,6 +1,9 @@
 #################
 # fstat function
 #################
+#
+# Wrapper for fst estimator from hierfstat package
+#
 fstat <- function(x, pop=NULL, fstonly=FALSE){
     ## misc checks
     if(!is.genind(x)) stop("x is not a valid genind object")
@@ -15,5 +18,27 @@ fstat <- function(x, pop=NULL, fstonly=FALSE){
     res <- varcomp.glob(levels=data.frame(pop), loci=dat)$F
 
     if(fstonly) {res <- res[1,1]}
+    return(res)
+}
+
+
+
+###############
+# fst function
+###############
+#
+# classical fst sensu Weir 1996 Genetic data analysis II pp. 166-167
+#
+fst <- function(x, pop=NULL){
+    ## misc checks
+    if(!is.genind(x)) stop("x is not a valid genind object")
+    if(!require(hierfstat)) stop("hierfstat package is required. Please install it.")
+    
+    if(is.null(pop)) pop <- x@pop
+    if(is.null(pop)) stop("no pop factor provided")
+    if(length(pop)!=nrow(x@tab)) stop("pop has a wrong length.")
+
+    ## computations
+    
     return(res)
 }
