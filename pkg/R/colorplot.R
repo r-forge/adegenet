a0b355b1948f3eaa5ccb49cf99b0d7ccdd100adb
jombart (Mr ) <t.jombart@imperial.ac.uk>
2008-09-10 13:46:31 +0000

    Added a first draft of colorplot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@174 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/colorplot.R
@@ -0,0 +1,34 @@
+colorplot <- function(xy, X, axes=1:ncol(X), add.plot=TRUE, ...){
+
+    ## some checks
+    if(any(is.na(xy))) stop("NAs exist in xy")
+    if(!is.numeric(xy)) stop("xy is not numeric")
+    if(nrow(xy) != nrow(X)) stop("xy and X have different row numbers")
+    X <- as.matrix(X[,axes,drop=FALSE])
+    if(any(is.na(X))) stop("NAs exist in X")
+    if(!is.numeric(X)) stop("X is not numeric")
+
+
+    ## function mapping x to [0,+inf[
+    f1 <- function(x){
+        x <- x + abs(min(x))
+        return(x)
+    }
+
+    ## apply f1 to X
+    X <- apply(X, 2, f1)
+
+    v1 <- X[,1]
+    if(ncol(X)==2) {v2 <- X[,2]} else {v2 <- 0}
+    if(ncol(X)==3) {v2 <- X[,3]} else {v3 <- 0}
+
+    ## find the colors
+    col <- rgb(v1, v2, v3, maxColorValue=max(X))
+
+    ## plot data
+    if(!add.plot) {
+        plot(xy, pch=20, col=col, ...)
+    } else {
+        points(xy, pch=20, col=col, ...)
+    }
+}
