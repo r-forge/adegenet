0c7a15f72f08192e36c07ddee1731b64344a06d8
jombart (Mr ) <t.jombart@imperial.ac.uk>
2008-09-14 13:22:27 +0000

    colorplot implemented ; check is Ok with R 2.7.1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/colorplot.R
+++ pkg/R/colorplot.R
@@ -1,7 +1,29 @@
-colorplot <- function(xy, X, axes=1:ncol(X), add.plot=FALSE, defaultLevel=0, ...){
+##
+## COLOR PLOT
+##
+## used to plot up to 3 variables in space using RGB system
+##
+## all coded in S3 method (arguments vary largely)
+##
+
+
+##########
+# generic
+##########
+colorplot <- function(...){
+    UseMethod("colorplot")
+}
+
+
+
+#################
+# default method
+#################
+colorplot.default <- function(xy, X, axes=1:ncol(X), add.plot=FALSE, defaultLevel=0, ...){
 
     ## some checks
     if(any(is.na(xy))) stop("NAs exist in xy")
+    xy <- as.matrix(xy)
     if(!is.numeric(xy)) stop("xy is not numeric")
     if(nrow(xy) != nrow(X)) stop("xy and X have different row numbers")
     X <- as.matrix(X[,axes,drop=FALSE])
@@ -11,7 +33,9 @@ colorplot <- function(xy, X, axes=1:ncol(X), add.plot=FALSE, defaultLevel=0, ...
 
     ## function mapping x to [0,+inf[
     f1 <- function(x){
-        x <- x + abs(min(x))
+        if(any(x<0)) {
+            x <- x + abs(min(x))
+        }
         return(x)
     }
 
@@ -19,16 +43,26 @@ colorplot <- function(xy, X, axes=1:ncol(X), add.plot=FALSE, defaultLevel=0, ...
     X <- apply(X, 2, f1)
 
     v1 <- X[,1]
-    if(ncol(X)==2) {v2 <- X[,2]} else {v2 <- defaultLevel}
-    if(ncol(X)==3) {v2 <- X[,3]} else {v3 <- defaultLevel}
+    if(ncol(X)>=2) {v2 <- X[,2]} else {v2 <- defaultLevel}
+    if(ncol(X)>=3) {v3 <- X[,3]} else {v3 <- defaultLevel}
 
-    ## find the colors
+    ## make colors
     col <- rgb(v1, v2, v3, maxColorValue=max(X))
 
+    ## handle ...
+    listArgs <- list(...)
+    if(is.null(listArgs$pch)) {listArgs$pch <- 20}
+
+    ## build list of arguments
+    listArgs$x <- xy
+    listArgs$col <- col
+    
     ## plot data
     if(!add.plot) {
-        plot(xy, pch=20, col=col, ...)
+        do.call(plot,listArgs)
     } else {
-        points(xy, pch=20, col=col, ...)
+        do.call(points,listArgs)
     }
-}
+
+    return(invisible(match.call()))
+} # end colorplot.default
