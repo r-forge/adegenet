e60afc4c265d9d50631962402aa569b3cea16c94
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-04 18:46:10 +0000

    added drop argument to seppop.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/handling.R
+++ pkg/R/handling.R
@@ -312,7 +312,7 @@ setMethod("[","genpop",
 setGeneric("seppop", function(x, ...) standardGeneric("seppop"))
 
 ## genind
-setMethod("seppop", signature(x="genind"), function(x,pop=NULL,truenames=TRUE,res.type=c("genind","matrix")){
+setMethod("seppop", signature(x="genind"), function(x,pop=NULL,truenames=TRUE,res.type=c("genind","matrix"), drop=FALSE){
     checkType(x)
 
     ## misc checks
@@ -332,7 +332,7 @@ setMethod("seppop", signature(x="genind"), function(x,pop=NULL,truenames=TRUE,re
     ## pop <- x@pop # comment to take pop arg into account
 
     ## make a list of genind objects
-    kObj <- lapply(levels(pop), function(lev) x[pop==lev, ])
+    kObj <- lapply(levels(pop), function(lev) x[pop==lev, , drop=drop])
     names(kObj) <- levels(pop)
 
     ## res is a list of genind
