1f360f8c22a633297100ac2f8ad054546a96c38c
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-08 10:18:10 +0000

    Fixed an issue arising when one pop is fixed at one locus.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@658 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Hs.R
+++ pkg/R/Hs.R
@@ -3,12 +3,22 @@
 ############################
 Hs <- function(x, truenames=TRUE) {
 
-    ## checks
+    ## CHECKS
     if(!is.genpop(x)) stop("x is not a valid genpop object")
     if(x@type=="PA") stop("not implemented for presence/absence markers")
 
-    ## main computations
+
+    ## MAIN COMPUTATIONS
+
+    ## have to handle loci with no polymorphism
     x.byloc <- seploc(x, truenames=truenames)
+
+    toRemove <- which(x@loc.nall==1)
+      if(length(toRemove)>0){
+          x.byloc <- x.byloc[-toRemove]
+
+    }
+
     lX <- lapply(x.byloc, function(e) makefreq(e, quiet=TRUE, truenames=truenames)$tab)
     lres <- lapply(lX, function(X) 1- apply(X^2,1,sum))
 
