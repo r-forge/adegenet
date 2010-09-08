59541179d2dfb86f4db2dd566141bafdb1825d1a
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-08 10:54:34 +0000

    Now the bug with fixed loci (in all pop of a genpop) is fixed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@659 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Hs.R
+++ pkg/R/Hs.R
@@ -9,20 +9,11 @@ Hs <- function(x, truenames=TRUE) {
 
 
     ## MAIN COMPUTATIONS
-
-    ## have to handle loci with no polymorphism
     x.byloc <- seploc(x, truenames=truenames)
-
-    toRemove <- which(x@loc.nall==1)
-      if(length(toRemove)>0){
-          x.byloc <- x.byloc[-toRemove]
-
-    }
 
     lX <- lapply(x.byloc, function(e) makefreq(e, quiet=TRUE, truenames=truenames)$tab)
     lres <- lapply(lX, function(X) 1- apply(X^2,1,sum))
-
     res <- apply(as.matrix(data.frame(lres)),1,mean)
-
+ 
     return(res)
 } # end Hs
