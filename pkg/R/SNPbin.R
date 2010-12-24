1694aa10be2205405e3b294536553b29f5024ebf
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-24 17:06:03 +0000

    Adapted the class for any level of ploidy.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@745 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/SNPbin.R
+++ pkg/R/SNPbin.R
@@ -9,11 +9,12 @@
 ###############
 ## SNPbin class
 ###############
-setClass("SNPbin", representation(snp = "raw",
+setClass("SNPbin", representation(snp = "list",
                                   n.loc = "integer",
                                   NA.posi = "integer",
-                                  label = "charOrNULL"),
-         prototype(snp = raw(0), n.loc = integer(0), label = NULL))
+                                  label = "charOrNULL",
+                                  ploidy = "integer"),
+         prototype(snp = list(0), n.loc = integer(0), label = NULL, ploidy = 1L))
 
 
 
@@ -24,8 +25,9 @@ setClass("SNPbin", representation(snp = "raw",
 setClass("genlight", representation(tab = "list",
                                     n.loc = "integer",
                                     ind.names = "charOrNULL",
-                                    loc.names = "charOrNULL"),
-         prototype(tab = list(0), n.loc = integer(0), ind.names = NULL, loc.names = NULL))
+                                    loc.names = "charOrNULL",
+                                    ploidy = "integer"),
+         prototype(tab = list(0), n.loc = integer(0), ind.names = NULL, loc.names = NULL, ploidy = 1L))
 
 
 
@@ -51,21 +53,53 @@ setMethod("initialize", "SNPbin", function(.Object, ...) {
 
     ## handle snp data ##
     if(!is.null(input$snp)){
+        ## a vector of raw is provided
         if(is.raw(input$snp)){
-            x@snp <-input$snp
+            x@snp <-list(input$snp)
         }
 
+        ## a list of raw vectors is provided
+        if(is.list(input$snp)){
+            if(all(sapply(input$snp, class)=="raw")){
+                x@snp <- input$snp
+            }
+        }
+
+        ## a numeric/integer vector is provided
         ## conversion from a vector of 0/1 (integers)
         if(is.numeric(input$snp) | is.integer(input$snp)){
-            temp <- na.omit(unique(input$snp))
-            if(!all(temp %in% c(0,1))){
-                stop("Values of SNPs are not all 0, 1, or NA")
+            input$snp <- as.integer(input$snp)
+            ## determine ploidy
+            if(is.null(input$ploidy)){
+                input$ploidy <- max(input$snp, na.rm=TRUE)
+            }
+            input$ploidy <- as.integer(input$ploidy)
+            if(input$ploidy<1) stop("Ploidy is less than 1")
+
+            ## check values in the vector
+            if(any(input$snp<0 | input$snp>input$ploidy, na.rm=TRUE)){
+                stop("Values of SNPs < 0 or > ploidy")
             }
 
-            temp <- .bin2raw(input$snp)
-            x@snp <- temp$snp
-            x@n.loc <- temp$n.loc
-            x@NA.posi <- temp$NA.posi
+
+            ## handle ploidy (may have to split info into binary vectors)
+            x@snp <- list()
+            i <- max(input$snp, na.rm=TRUE) # determine max nb of alleles
+            if(i > 1){ # haplotype can be 0/1/2/...
+                j <- 0 # index for the length of the list @snp
+                while(i > 0){
+                    j <- j+1 # update length of the result
+                    temp <- as.integer(input$snp==i)
+                    x@snp[[j]] <- .bin2raw(temp)$snp # make a vector of 1
+                    input$snp <- input$snp-temp # deflate data (subtract the recoded alleles)
+                    i <- max(input$snp, na.rm=TRUE) # update the max nb of alleles
+                }
+            } else { # haplotype is only 0/1/NA
+                x@snp[[1]] <- .bin2raw(input$snp)$snp
+            }
+            x@n.loc <- length(input$snp)
+            x@NA.posi <- which(is.na(input$snp))
+            x@ploidy <- input$ploidy
             return(x)
         }
     }
@@ -82,6 +116,12 @@ setMethod("initialize", "SNPbin", function(.Object, ...) {
     ## handle NA.posi ##
     if(!is.null(input$NA.posi)){
         x@NA.posi <- as.integer(input$NA.posi)
+    }
+
+
+    ## handle ploidy ##
+    if(!is.null(input$ploidy)){
+        x@ploidy <- as.integer(input$ploidy)
     }
 
     return(x)
@@ -111,12 +151,13 @@ setMethod("initialize", "SNPbin", function(.Object, ...) {
 ###############
 setMethod ("show", "SNPbin", function(object){
     cat(" === S4 class SNPbin ===")
-    if(!is.null(x@label)) {
-        cat("\n", x@label)
+    if(!is.null(object@label)) {
+        cat("\n", object@label)
     }
     cat("\n", nLoc(object), "SNPs coded as bits")
+    cat("\n Ploidy:", object@ploidy)
     temp <- round(length(object@NA.posi)/nLoc(object) *100,2)
-    cat("\n", length(object@NA.posi), " (", temp," %) missing data\n", sep="")
+    cat("\n ", length(object@NA.posi), " (", temp," %) missing data\n", sep="")
 }) # end show method
 
 
@@ -126,6 +167,11 @@ setMethod ("show", "SNPbin", function(object){
 ## accessors
 ############
 setMethod("nLoc","SNPbin", function(x,...){
+    return(x@n.loc)
+})
+
+
+setMethod("nLoc","genlight", function(x,...){
     return(x@n.loc)
 })
 
@@ -160,7 +206,7 @@ setMethod("names", signature(x = "genlight"), function(x){
 setMethod("[", signature(x="SNPbin", i="ANY"), function(x, i) {
     if (missing(i)) i <- TRUE
     temp <- .SNPbin2int(x) # data as integers with NAs
-    x <- new("SNPbin", snp=temp[i], label=x@label)
+    x <- new("SNPbin", snp=temp[i], label=x@label, ploidy=x@ploidy)
     return(x)
 }) # end [] for SNPbin
 
@@ -182,9 +228,7 @@ setMethod("[", signature(x="SNPbin", i="ANY"), function(x, i) {
 ## each byte takes a value on [0,255]
 
 ## function to code multiple SNPs on a byte
-## 8 combinations of SNPs can be coded on a single byte
-## we use bytes values from [1,128]
-## 200 is a missing value
+## 8 combinations of SNPs can be coded onto a single byte (0->255)
 .bin2raw <- function(vecSnp){
     ## was required in pure-R version
     ## SNPCOMB <- as.matrix(expand.grid(rep(list(c(0,1)), 8)))
@@ -221,15 +265,28 @@ setMethod("[", signature(x="SNPbin", i="ANY"), function(x, i) {
 
 
 
+###########
+## .raw2bin
+###########
+## convert vector of raw to 0/1 integers
+.raw2bin <- function(x){
+    SNPCOMB <- as.matrix(expand.grid(rep(list(c(0,1)), 8)))
+    colnames(SNPCOMB) <- NULL
+    res <- unlist(lapply(as.integer(x), function(i) SNPCOMB[i+1,]))
+} # end .raw2bin
+
+
+
+
+
 #############
 ## .SNPbin2int
 #############
 ## convert SNPbin to integers (0/1)
 .SNPbin2int <- function(x){
-    SNPCOMB <- as.matrix(expand.grid(rep(list(c(0,1)), 8)))
-    colnames(SNPCOMB) <- NULL
-    res <- unlist(lapply(as.integer(x@snp), function(i) SNPCOMB[i+1,]))
-    res <- res[1:x@n.loc]
+    res <- lapply(x@snp, .raw2bin)
+    res <- lapply(res, function(e) e[1:x@n.loc])
+    res <- as.integer(Reduce("+", res))
     if(length(x@NA.posi)>0){
         res[x@NA.posi] <- NA
     }
@@ -268,8 +325,33 @@ as.integer.SNPbin <- function(x, ...){
 ################################
 ## testing :
 ##
-## dat <- sample(c(0,1,NA), 1e6, prob=c(.5, .495, .005), replace=TRUE)
+## HAPLOID DATA
+##
+## library(adegenet)
+
+## dat <- sample(c(0L,1L,NA), 1e6, prob=c(.5, .495, .005), replace=TRUE)
 ## x <- new("SNPbin", dat)
+
+## identical(as(x, "integer"),dat) # SHOULD NORMALLY BE TRUE
+## all(as(x, "integer") == dat, na.rm=TRUE) # MUST BE TRUE
+
+## object.size(dat)/object.size(x) # EFFICIENCY OF CONVERSION
+
+
+
+ ## DIPLOID DATA
+## dat <- sample(c(0:2,NA), 1e6, prob=c(.4, .4, .195 ,.005), replace=TRUE)
+## x <- new("SNPbin", dat)
+
+## identical(as(x, "integer"),dat) # MUST BE TRUE
+
+## object.size(dat)/object.size(x) # EFFICIENCY OF CONVERSION
+
+
+
+ ## POLYLOID DATA
+## dat <- sample(c(0:5,NA), 1e6, prob=c(rep(.995/6,6), 0.005), replace=TRUE)
+##  x <- new("SNPbin", dat)
 
 ## identical(as(x, "integer"),dat) # MUST BE TRUE
 
