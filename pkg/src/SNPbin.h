88f21e0e1d059a4893780d5c5d71d0d47c0f3f1a
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-08 14:51:16 +0000

    moving forward into the class mirroring in C.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@790 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/SNPbin.h
+++ pkg/src/SNPbin.h
@@ -9,7 +9,9 @@
 /* EXTERNAL */
 void binIntToBytes(int *vecsnp, int *vecsize, unsigned char *vecres, int *ressize);
 void bytesToBinInt(unsigned char *vecbytes, int *vecsize, int *vecres);
+void bytesToInt(unsigned char *vecbytes, int *veclength, int *nbvec, int *vecres);
 
 /* INTERNAL */
 void byteToBinInt(unsigned char in, int out[8]);
 void testRaw(unsigned char *a, int *n);
+double snpbin_dotprod(struct snpbin *x, struct snpbin *y, double *mean, double*sd);
