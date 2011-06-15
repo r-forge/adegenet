b66a8213866970b2fc64657c13deca203cebfb40
jombart (Mr ) <t.jombart@imperial.ac.uk>
2011-06-15 15:05:04 +0000

    A few changes on inbreeding.ml, which now is the only inbreeding function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@912 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/inbreeding.R
+++ pkg/R/inbreeding.R
@@ -1,82 +1,82 @@
-#############
-## inbreeding
-#############
-inbreeding <- function(x, pop=NULL, truenames=TRUE, res.type=c("mean","byloc"), plot=TRUE, ...){
-    ## CHECKS ##
-    if(!is.genind(x)) stop("x is not a valid genind object")
-    checkType(x)
-    res.type <- match.arg(res.type)
+## #############
+## ## inbreeding
+## #############
+## inbreeding <- function(x, pop=NULL, truenames=TRUE, res.type=c("mean","byloc"), plot=TRUE, ...){
+##     ## CHECKS ##
+##     if(!is.genind(x)) stop("x is not a valid genind object")
+##     checkType(x)
+##     res.type <- match.arg(res.type)
 
-    if(x$ploidy != 2) stop("this inbreeding coefficient is designed for diploid genotypes only")
+##     if(x$ploidy != 2) stop("this inbreeding coefficient is designed for diploid genotypes only")
 
-    if(!is.null(pop)) pop(x) <- pop
-    if(is.null(x@pop) && is.null(pop)) {
-        pop(x) <- factor(rep(1, nrow(x@tab)))
-    }
-
-
-    ## COMPUTATIONS ##
-
-    ## get allele frequencies and \sum p_i^2 by pop and loc ##
-    tabfreq2 <- (makefreq(x = genind2genpop(x, quiet = TRUE), quiet=TRUE, truenames=truenames)$tab) ^2
-    sumpi2 <- t(apply(tabfreq2, 1, tapply, x$loc.fac, sum))
-
-    ## function to check a 1-locus genotype for homozigosity
-    ## returns 1 if homoz, 0 otherwise
-    ## !!! NOTE : reverse the values returned by f1 to obtain a strange thing !!!
-    f1 <- function(gen){
-        if(any(is.na(gen))) return(NA)
-        if(any(round(gen, 10)==1)) return(1)
-        return(0)
-    }
-
-    ## get the table of binary hetero/homo data
-    if(truenames) {
-        X <- truenames(x)$tab
-    } else
-    X <- x$tab
-
-    homotab <- t(apply(X, 1, tapply, x@loc.fac, f1))
+##     if(!is.null(pop)) pop(x) <- pop
+##     if(is.null(x@pop) && is.null(pop)) {
+##         pop(x) <- factor(rep(1, nrow(x@tab)))
+##     }
 
 
-    ## get pi2 for the appropriate pop
-    if(truenames){
-    popx <- pop(x)
-    } else {
-        popx <- x$pop
-    }
+##     ## COMPUTATIONS ##
 
-    popx <- as.character(popx)
-    tabpi2 <- sumpi2[popx, , drop=FALSE]
+##     ## get allele frequencies and \sum p_i^2 by pop and loc ##
+##     tabfreq2 <- (makefreq(x = genind2genpop(x, quiet = TRUE), quiet=TRUE, truenames=truenames)$tab) ^2
+##     sumpi2 <- t(apply(tabfreq2, 1, tapply, x$loc.fac, sum))
+
+##     ## function to check a 1-locus genotype for homozigosity
+##     ## returns 1 if homoz, 0 otherwise
+##     ## !!! NOTE : reverse the values returned by f1 to obtain a strange thing !!!
+##     f1 <- function(gen){
+##         if(any(is.na(gen))) return(NA)
+##         if(any(round(gen, 10)==1)) return(1)
+##         return(0)
+##     }
+
+##     ## get the table of binary hetero/homo data
+##     if(truenames) {
+##         X <- truenames(x)$tab
+##     } else
+##     X <- x$tab
+
+##     homotab <- t(apply(X, 1, tapply, x@loc.fac, f1))
 
 
-    ## COMPUTE FINAL RESULT ##
-    num <- homotab - tabpi2
-    ## denom <- tabpi2 * (1 - tabpi2) # does not actually compute a weighted mean
-    denom <- 1 - tabpi2
-    res <- num / denom
-    ## return values per locus ##
-    if(res.type=="byloc") return(res)
+##     ## get pi2 for the appropriate pop
+##     if(truenames){
+##     popx <- pop(x)
+##     } else {
+##         popx <- x$pop
+##     }
 
-    ## return mean weighted by effective nb of alleles ##
-    wtab <- 1/tabpi2
-    wtab[is.na(res)] <- NA
-    wtab <- t(apply(wtab, 1, function(e) return(e/sum(e,na.rm=TRUE))))
-    res <- wtab * res
+##     popx <- as.character(popx)
+##     tabpi2 <- sumpi2[popx, , drop=FALSE]
 
-    res <- apply(res, 1, sum, na.rm=TRUE)
-    if(plot){
-        par(bg="grey")
-        nPop <- length(unique(popx))
-        myCol <- rainbow(nPop)[as.integer(pop(x))]
-        if(min(res)>0) ylim <- c(0, 1.1*max(res))
-        if(max(res)<0) ylim <- c(min(res), 0+abs(min(res))*0.1)
-        plot(res, col=myCol, type="h", ylab="Inbreeding", xlab="Individuals", ...)
-    }
 
-    return(res)
+##     ## COMPUTE FINAL RESULT ##
+##     num <- homotab - tabpi2
+##     ## denom <- tabpi2 * (1 - tabpi2) # does not actually compute a weighted mean
+##     denom <- 1 - tabpi2
+##     res <- num / denom
+##     ## return values per locus ##
+##     if(res.type=="byloc") return(res)
 
-} # end inbreeding
+##     ## return mean weighted by effective nb of alleles ##
+##     wtab <- 1/tabpi2
+##     wtab[is.na(res)] <- NA
+##     wtab <- t(apply(wtab, 1, function(e) return(e/sum(e,na.rm=TRUE))))
+##     res <- wtab * res
+
+##     res <- apply(res, 1, sum, na.rm=TRUE)
+##     if(plot){
+##         par(bg="grey")
+##         nPop <- length(unique(popx))
+##         myCol <- rainbow(nPop)[as.integer(pop(x))]
+##         if(min(res)>0) ylim <- c(0, 1.1*max(res))
+##         if(max(res)<0) ylim <- c(min(res), 0+abs(min(res))*0.1)
+##         plot(res, col=myCol, type="h", ylab="Inbreeding", xlab="Individuals", ...)
+##     }
+
+##     return(res)
+
+## } # end inbreeding
 
 
 
@@ -95,7 +95,8 @@ inbreeding.ml <- function(x, pop=NULL, truenames=TRUE, res.type=c("sample","func
     checkType(x)
     res.type <- match.arg(res.type)
 
-    if(x$ploidy != 2) stop("this inbreeding coefficient is designed for diploid genotypes only")
+    ## if(x$ploidy != 2) stop("this inbreeding coefficient is designed for diploid genotypes only")
+    PLO <- ploidy(x)
 
     if(!is.null(pop)) pop(x) <- pop
     if(is.null(x@pop) && is.null(pop)) {
@@ -105,7 +106,9 @@ inbreeding.ml <- function(x, pop=NULL, truenames=TRUE, res.type=c("sample","func
       ## COMPUTATIONS ##
 
     ## get allele frequencies and \sum p_i^2 by pop and loc ##
-    tabfreq2 <- (makefreq(x = genind2genpop(x, quiet = TRUE), quiet=TRUE, truenames=truenames)$tab) ^2
+    ## (generalized to any ploidy) ##
+    ## tabfreq2 <- (makefreq(x = genind2genpop(x, quiet = TRUE), quiet=TRUE, truenames=truenames)$tab) ^2
+    tabfreq2 <- (makefreq(x = genind2genpop(x, quiet = TRUE), quiet=TRUE, truenames=truenames)$tab) ^ PLO
     sumpi2 <- t(apply(tabfreq2, 1, tapply, x$loc.fac, sum))
 
     ## function to check a 1-locus genotype for homozigosity
