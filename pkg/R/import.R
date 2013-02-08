b0cf29f2a6cf3b5d2569b8c61aa6f2445fe7b5fe
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-08 18:05:08 +0000

    fixed an issue in fasta2genlight when there was no SNPs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adegenet@1071 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/import.R
+++ pkg/R/import.R
@@ -1111,6 +1111,10 @@ fasta2genlight <- function(file, quiet=FALSE, chunkSize=1000, saveNbAlleles=FALS
     ## POOL <- lapply(POOL, setdiff, "-")
     nb.alleles <- sapply(POOL, length)
     snp.posi <- nb.alleles==2
+    if(all(!snp.posi)){
+        warning("No polymorphism in the alignment - returning empty object")
+        return(new("genlight"))
+    }
     sec.all <- unlist(lapply(POOL[snp.posi], function(e) e[2]))
 
 
